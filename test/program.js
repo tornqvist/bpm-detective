58b94d3c818082c465d7a10834d7a0abcd0e69b2
Carl Törnqvist <calle.tornqvist@gmail.com>
2015-08-31 14:12:12 +0200

    Made tests server independent by inlining wav as base64

--- test/program.js
+++ test/program.js
@@ -1,40 +1,65 @@
+import 'babel/polyfill';
+import 'whatwg-fetch';
 import test from 'tape';
 import detect from './../lib/detect';
 
 const AudioContext = (window.AudioContext || window.webkitAudioContext);
 
+/**
+ * Load raw wav files encoded in base64
+ */
+
+// Needed to circumvent Babel's transformation
+let fs = require('fs');
+let fixtures = {
+  '90': fs.readFileSync(__dirname + '/fixtures/90bpm.wav', { encoding: 'base64' }),
+  '97': fs.readFileSync(__dirname + '/fixtures/97bpm.wav', { encoding: 'base64' }),
+  '117': fs.readFileSync(__dirname + '/fixtures/117bpm.wav', { encoding: 'base64' }),
+  '140': fs.readFileSync(__dirname + '/fixtures/140bpm.wav', { encoding: 'base64' }),
+  '170': fs.readFileSync(__dirname + '/fixtures/170bpm.wav', { encoding: 'base64' })
+};
+
 test('detects bpm', assert => {
   const beats = [97, 117, 140, 170];
 
-  assert.plan(beats.length);
-
   // Load beats
-  Promise.all(beats.map(loadBeat))
+  Promise.all(beats.map(bpm => convert(fixtures[bpm])))
     // Detect bpm of all beats
-    .then((sources) => Promise.all(sources.map(detect)))
-    .catch(assert.end)
+    .then(sources => Promise.all(sources.map(detect)))
     // Match detected bpm to expected bpm
     .then(results => results.map((bpm, index) => [bpm, beats[index]]))
     // Map pairs through assert
-    .then(pairs => pairs.forEach(pair => assert.equal(...pair)));
+    .then(pairs => pairs.forEach(pair => assert.equal(...pair)))
+    .then(assert.end, assert.end);
 });
 
 test('fails with short sample', assert => {
-  loadBeat(90)
+  convert(fixtures['90'])
     .then(detect)
     .then(bpm => assert.fail(`Detected ${ bpm }`))
     .catch(err => assert.pass(err.message))
     .then(assert.end);
 });
 
-function loadBeat(bpm) {
-  let url = `fixtures/${ bpm }bpm.wav`;
-  let context = new AudioContext();
+/**
+ * Convert base64 string into AudioBuffer
+ */
 
-  return new Promise((resolve, reject) => {
-    return fetch(url)
-      .then(response => response.arrayBuffer())
-      .then(buffer => context.decodeAudioData(buffer, resolve, reject))
-      .catch(reject);
-  });
+function convert(base64) {
+  let context = new AudioContext();
+  let binaryString = atob(base64);
+  let len = binaryString.length;
+  let bytes = new Uint8Array(len);
+
+  for (let i = 0; i < len; i += 1)        {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+
+  try {
+    return context.decodeAudioData(bytes.buffer);
+  } catch (err) {
+    return new Promise((resolve, reject) => {
+      return context.decodeAudioData(bytes.buffer, resolve, reject);
+    });
+  }
 }
